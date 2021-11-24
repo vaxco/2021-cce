eda8e225268c8c46048627b5f59947fbcc0641aa
王證樺 <91170653+vaxco@users.noreply.github.com>
2021-11-24 11:44:13 +0800

    Add files via upload

---
+++ week11/week11-1.cpp
@@ -0,0 +1,24 @@
+///Week11-1.cpp step01-1
+#include <stdio.h>
+int a[10000000];
+int main()
+{
+	int m;
+	scanf("%d",&m);
+
+	///int ans=0;
+	for(int i=2;i<m;i++){
+		if(a[i]==0){
+			///ans++;
+			printf("%d ",i);
+			for(int k=i+i;k<m;k=k+i){
+			a[k]=1;
+
+            }
+
+        }
+	}
+
+	///printf("%d\n",ans);
+
+}
---
+++ week11/week11-2.cpp
@@ -0,0 +1,25 @@
+///Week11-2.cpp step02-1
+#include <stdio.h>
+int a[10000000];
+int main()
+{
+    printf("請問你想要幾個質數?(最大質數不超過10000000):");
+	int Q;
+	scanf("%d",&Q);
+
+	int ans=0;
+	for(int i=2;ans<Q;i++){
+		if(a[i]==0){
+			ans++;
+			printf("%d ",i);
+			for(int k=i+i;k<10000000;k=k+i){
+			a[k]=1;
+
+            }
+
+        }
+	}
+
+	///printf("%d\n",ans);
+
+}
